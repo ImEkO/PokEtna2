4627c7e5cb75f6b10d698225794fa16e280496fc
ImEkO <olivie_a@etna-alternance.net>
2017-04-14 15:51:06 +0200

    Vendredi 14/04 Recup des nom des pokemons dans l'api dans la variable list recup de toute les info du pokemon dans currents recup de charque caract dans current Tous dans le fichier ScrollingActivity

--- app/src/main/java/com/example/stevenolivier/poketna/ScrollingActivity.java
+++ app/src/main/java/com/example/stevenolivier/poketna/ScrollingActivity.java
@@ -7,12 +7,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -24,30 +31,41 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         URL url;
         HttpURLConnection urlConnection = null;
-        Log.d("1", "1");
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        String currents;
         try {
-            Log.d("2", "2");
             url = new URL("http://pokeapi.co/api/v2/pokemon/");
             urlConnection = (HttpURLConnection) url.openConnection();
-            Log.d("3", "3");
             InputStream in = urlConnection.getInputStream();
-            Log.d("4", "4");
             InputStreamReader isw = new InputStreamReader(in);
+            currents = " ";
             int data = isw.read();
             while (data != -1) {
                 char current = (char) data;
                 data = isw.read();
-                Log.d("test", "valeur : " + current);
-                }
+                currents += current;
+            }
+            JSONObject obj = new JSONObject(currents);
+            List<String> list = new ArrayList<String>();
+            JSONArray array = obj.getJSONArray("results");
+            for (int i = 0; i < array.length(); i++) {
+                list.add(array.getJSONObject(i).getString("name"));
+                Button button = new Button(this);
+                button.setText(array.getJSONObject(i).getString("name"));
+                button.setTextSize(20);
+                button.setGravity(Gravity.CENTER);
+                //this.addView(button);
+            }
+            Log.d("Toute es info", "valeur : " + currents);
+            Log.d("Seulement les noms", "valeur : " + list);
             }
             catch (Exception e) {
             e.printStackTrace();
-            } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
         }
-        }
-    }
 }
