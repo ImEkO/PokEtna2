01e69c60a760d6e1ffa8a5bb457b97eace7f11c1
ImEkO <olivie_a@etna-alternance.net>
2017-04-18 10:54:39 +0200

    Vendredi 14/04 Recup des nom des pokemons dans l'api dans la variable list recup de toute les info du pokemon dans currents recup de charque caract dans current Tous dans le fichier ScrollingActivity

--- app/src/main/java/com/example/stevenolivier/poketna/Mainvue.java
+++ app/src/main/java/com/example/stevenolivier/poketna/Mainvue.java
@@ -16,7 +16,6 @@ public class Mainvue extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.pokemon);
         Button btn1 = (Button) findViewById(R.id.berry);
         Button btn2 = (Button) findViewById(R.id.item);
-        Button btn3 = (Button) findViewById(R.id.localisation);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,13 +35,6 @@ public class Mainvue extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Mainvue.this, ScrollingActivity2.class));
-            }
-
-        });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Mainvue.this, ScrollingActivity3.class));
             }
 
         });
