efbd8a7ffeafa6a12bdab57b33dba1ea842f866d
ImEkO <olivie_a@etna-alternance.net>
2017-04-20 11:00:09 +0200

    debug

--- app/src/main/java/com/example/stevenolivier/poketna/ScrollingActivity.java
+++ app/src/main/java/com/example/stevenolivier/poketna/ScrollingActivity.java
@@ -1,6 +1,9 @@
 package com.example.stevenolivier.poketna;
 
 import android.app.DownloadManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 
@@ -9,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -23,28 +27,40 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    protected ImageView img;
+    protected LinearLayout layout;
+    protected String urlimgp;
+    protected Button button;
+    protected int i = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        URL url;
+        layout = (LinearLayout) findViewById(R.id.test);
+        onCreatepb();
+    }
+
+    protected void onCreatepb() {
+        /*URL url;
         HttpURLConnection urlConnection = null;
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.test);
         String currents;
         try {
+            img = new ImageView(this);
             url = new URL("http://pokeapi.co/api/v2/pokemon/");
             urlConnection = (HttpURLConnection) url.openConnection();
             InputStream in = urlConnection.getInputStream();
@@ -61,89 +77,69 @@ public class ScrollingActivity extends AppCompatActivity {
             JSONArray array = obj.getJSONArray("results");
             for (int i = 0; i < array.length(); i++) {
                 list.add(array.getJSONObject(i).getString("name"));
-                Button button = new Button(this);
-                button.setText(array.getJSONObject(i).getString("name"));
-                button.setTextSize(20);
-                button.setGravity(Gravity.CENTER);
-                layout.addView(button);
-                System.out.println("Sortie : " + array.getJSONObject(i).getString("name"));
-                Img(array.getJSONObject(i).getString("name"));
-            }
+                //button.setText(array.getJSONObject(i).getString("name"));
+                //button.setTextSize(20);
+                //button.setGravity(Gravity.CENTER);*/
+        while (i <= 20) {
+            Img();
+            i++;
+        }
+                //layout.addView(button);
+            /*}
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
             }
-        }
+        }*/
     }
 
-    protected void Img(String nom) {
+    protected void Img() {
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://pokeapi.co/api/v2/pokemon/" + nom + "/";
-
+        String url = "http://pokeapi.co/api/v2/pokemon/" + i;
+        //System.out.println("Le nom : " + nom + "\n");
+        System.out.println("icii");
+        System.out.println(i);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            JSONObject obj = new JSONObject(response);
-                            JSONObject array = (JSONObject) obj.get("sprites");
-                            Log.d("Seulement les ", "Images" + array.getString("front_default"));
+                            System.out.println("la");
+                            JSONObject obej = new JSONObject(response);
+                            JSONObject array = (JSONObject) obej.get("sprites");
+                            JSONArray aa = (JSONArray) obej.get("forms");
+
+                            System.out.println("Le nom : " + array.getString("front_default") + "\n");
+                            System.out.println("Le Prenom : " + aa.getJSONObject(0).getString("name") + "\n");
+
+                            urlimgp = array.getString("front_default");
+
+                            System.out.println("1" + urlimgp);
+                            URL urlimg = new URL(urlimgp);
+                            //Bitmap image = BitmapFactory.decodeStream(urlimg.openConnection().getInputStream());
+                            //img.setImageBitmap(image);
+                            //img.setBackground(new BitmapDrawable(getResources(), image));
+                            //button.setText(aa.getJSONObject(0).getString("name"));
+                            //button.setTextSize(20);
+                            //button.setGravity(Gravity.CENTER);
+                            //layout.addView(img);
+                            //layout.addView(button);
                         } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                System.out.println("error");
+                System.out.println("error Listerner");
             }
         });
         queue.add(stringRequest);
-
-        /*URL url;
-        HttpURLConnection urlConnection = null;
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        String currents;
-        LinearLayout layout = (LinearLayout) findViewById(R.id.test);
-        String urlBerry = "http://pokeapi.co/api/v2/pokemon/" + nom + "/";
-        String urlBerryImg = " ";
-        try {
-            url = new URL(urlBerry);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = urlConnection.getInputStream();
-            InputStreamReader isw = new InputStreamReader(in);
-            currents = " ";
-            int data = isw.read();
-            while (data != -1) {
-                char current = (char) data;
-                data = isw.read();
-                currents += current;
-            }
-            JSONObject obj = new JSONObject(currents);
-            if (!obj.isNull("sprites")) {
-                JSONObject array = obj.getJSONObject("sprites");
-                urlBerryImg = array.getString("front_default");
-                URL urlimg = new URL(urlBerryImg);
-                ImageView img = new ImageView(this);
-                Bitmap image = BitmapFactory.decodeStream(urlimg.openConnection().getInputStream());
-                img.setImageBitmap(image);
-                Button button = new Button(this);
-                Bitmap resized = Bitmap.createScaledBitmap(image, (int) (image.getWidth() * 0.8), (int) (image.getHeight() * 0.8), true);
-                button.setText(urlBerryImg);
-                img.setBackground(new BitmapDrawable(getResources(), resized));
-                button.setTextSize(20);
-                button.setGravity(Gravity.CENTER);
-                layout.addView(img);
-            }
-        } catch (Exception e) {
-            Log.d("ERROR sur l’url -> " + urlBerry, "Problème subvenu lors de la récupération de l’image du pokemon !");
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }*/
     }
 }
