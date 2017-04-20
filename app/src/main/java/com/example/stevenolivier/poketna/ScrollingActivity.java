93c9fe8c9ae92a081f5805d59127587eade5dc41
ImEkO <olivie_a@etna-alternance.net>
2017-04-20 14:20:04 +0200

    debug

--- app/src/main/java/com/example/stevenolivier/poketna/ScrollingActivity.java
+++ app/src/main/java/com/example/stevenolivier/poketna/ScrollingActivity.java
@@ -1,9 +1,11 @@
 package com.example.stevenolivier.poketna;
 
 import android.app.DownloadManager;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.PictureDrawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 
@@ -23,6 +25,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -44,6 +47,8 @@ public class ScrollingActivity extends AppCompatActivity {
     protected String urlimgp;
     protected Button button;
     protected int i = 1;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,33 +62,33 @@ public class ScrollingActivity extends AppCompatActivity {
     protected void Img() {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://pokeapi.co/api/v2/pokemon/" + i;
-        System.out.println(url);
-        System.out.println("icii");
-        System.out.println(i);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                     new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
                             try {
-                                System.out.println("la");
                                 JSONObject obej = new JSONObject(response);
                                 JSONObject array = (JSONObject) obej.get("sprites");
                                 JSONArray aa = (JSONArray) obej.get("forms");
 
                                 System.out.println("L'image " + array.getString("front_default") + "\n");
                                 System.out.println("Le Nom : " + aa.getJSONObject(0).getString("name") + "\n");
-
+                                String test = aa.getJSONObject(0).getString("name");
                                 urlimgp = array.getString("front_default");
+
                                 System.out.println("re L'image" + urlimgp);
+                                System.out.println(test);
                                 URL urlimg = new URL(urlimgp);
+                                button = new Button(ScrollingActivity.this);
                                 //Bitmap image = BitmapFactory.decodeStream(urlimg.openConnection().getInputStream());
+                                //Picasso.with(getApplicationContext()).load(urlimgp).into(img);
                                 //img.setImageBitmap(image);
                                 //img.setBackground(new BitmapDrawable(getResources(), image));
-                                //button.setText(aa.getJSONObject(0).getString("name"));
-                                //button.setTextSize(20);
-                                //button.setGravity(Gravity.CENTER);
+                                button.setText(test);
+                                button.setTextSize(20);
+                                button.setGravity(Gravity.CENTER);
                                 //layout.addView(img);
-                                //layout.addView(button);
+                                layout.addView(button);
                                 if (i <= 20 ) {
                                     i++;
                                     Img();
